4b58708658caa706fc1397b1a894cc7744473fa1
kinas45y5 <48494105+kinas45y5@users.noreply.github.com>
2022-04-09 22:41:40 +0200

    finalized thing

--- drawpixel/drawpixel.c
+++ drawpixel/drawpixel.c
@@ -1,22 +1,24 @@
 #include <SDL.h>
 
+/* this is bad */
+
 int main(int argc, char** args)
 {
-    // initialize sdl
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window* window = SDL_CreateWindow("pixel", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 320, 320, SDL_WINDOW_SHOWN);
+    SDL_Window* window;
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Renderer* renderer;
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_CreateWindowAndRenderer(300, 300, 0, &window, &renderer);
 
-    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 0);
+
+    SDL_RenderDrawPoint(renderer, 100, 100);
 
     SDL_RenderPresent(renderer);
 
     SDL_Delay(3000);
 
     return 0;
-
 }
